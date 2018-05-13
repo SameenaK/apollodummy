306b6d97610f4994dbf24a2708b00ed53d15fd5e
SameenaK <sameenakhan2018@u.northwestern.edu>
2018-05-13 16:07:29 -0500

    new repo for dummy project

---
+++ App.js
@@ -0,0 +1,81 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: "http://localhost:8080/graphql"
+  // uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+import gql from 'graphql-tag';
+
+client
+  .query({
+    query: gql`
+      {
+        president(name: "Abraham Lincoln") {
+    name
+    term
+    party
+  }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
